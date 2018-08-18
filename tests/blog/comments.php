58f25c48941f7a11d6786c56f6be7038655fc4e2
Buu Buu <k.so.6.dev@gmail.com>
2018-08-18 16:28:21 +0200

    tp blog basic over

--- tests/blog/comments.php
+++ tests/blog/comments.php
@@ -1,6 +1,20 @@
 <?php 
-//display article with comments
+    /*
+    this page is used to zoom on an article, to read its full content and access to its comments
+    */
 
+    //include my sql library
+    include("kso_sqllib.php");
+
+    //init sql connexion
+    try
+    {
+        $bdd = new PDO('mysql:host=localhost;dbname=test_ocr;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));//to get sql queries better errors
+    }
+    catch (Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
 ?>
 <!DOCTYPE html>
 <html lang="fr-FR">
@@ -18,20 +32,52 @@
             </header>
             
             <section>
-                <h2>Zoom sur l'article xxx</h2>
-                <article>
-                    <h1>Titre de l'article xxx</h1>
-                    <p>
-
-                    </p>
-                </article>
-                
-                <div id="comments">
-                    <h3>Commentaires</h3>
-                    <p>
-
-                    </p>
-                </div>
+                <?php
+                    if (isset($_GET['id']))
+                    {
+                        $id = (int) htmlspecialchars($_GET['id']);
+                        
+                        $query = $bdd->prepare('SELECT a.title as atitle, a.author as aauthor, a.id as aid, a.content as acontent, a.creation_date as acdate, 
+                                                a.modification_date as amdate, c.article_id, c.id as cid, c.creation_date as cdate, c.message as cmessage, c.nickname as cnick 
+                                                FROM article AS a LEFT JOIN comment AS c ON c.article_id = a.id WHERE a.id=:id');
+                        $query->execute(array(
+                            'id' => $id
+                        )) or die (print_r($bdd->errorInfo()));
+                        
+                        $rec = $query->fetch();
+                ?>
+                        <h2>Zoom sur l'article <?php echo htmlspecialchars($rec['aid']); ?></h2>
+                        <article>
+                            <h1><?php echo htmlspecialchars($rec['atitle']); ?></h1>
+                            <p>
+                                Auteur: <?php echo htmlspecialchars($rec['aauthor']); ?> <br>
+                                Date de création: <?php echo htmlspecialchars($rec['acdate']); ?> <br>
+                                Dernière date de modification: <?php echo htmlspecialchars($rec['amdate']); ?> <br>
+                                <span class="content">
+                                    <?php echo nl2br(htmlspecialchars($rec['acontent'])); ?>
+                                </span>
+                            </p>
+                        </article>
+                        
+                        <div id="comments">
+                            <h3>Commentaires</h3>
+                            <?php
+                            if ($rec['cmessage'] != '')
+                            {
+                            ?>
+                                <p>
+                                    Pseudo: <?php echo htmlspecialchars($rec['cnick']); ?> <br>
+                                    Date : <?php echo htmlspecialchars($rec['cdate']); ?> <br>
+                                    <?php echo nl2br(htmlspecialchars($rec['cmessage'])); ?>
+                                </p>
+                            <?php
+                            } 
+                            ?>                            
+                        </div>
+                <?php  
+                        $query->closeCursor();
+                    }
+                ?>
             </section>
             
             <footer>
--- tests/blog/index.php
+++ tests/blog/index.php
@@ -1,5 +1,20 @@
 <?php
-//display list of the 5 last articles
+    /*
+    this page is the blog main page. it displays a list of the 5 last articles
+    */
+
+    //include my sql library
+    include("kso_sqllib.php");
+
+    //init sql connexion
+    try
+    {
+        $bdd = new PDO('mysql:host=localhost;dbname=test_ocr;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));//to get sql queries better errors
+    }
+    catch (Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
 ?>
 <!DOCTYPE html>
 <html lang="fr-FR">
@@ -16,12 +31,29 @@
             
             <section>
                 <h3>Liste des articles</h3>
-                <article>
-                    <h1>Titre de l'article xxx</h1>
-                    <p>
-                        <a href="comments.php" title="comments page"> <em>Commentaires</em> </a>                        
-                    </p>
-                </article>
+                <?php
+                    $query = $bdd->query('SELECT * FROM article ORDER BY creation_date DESC') or die (print_r($bdd->errorInfo()));
+                    while ($rec = $query->fetch())
+                    {
+                ?>
+
+                        <article>
+                            <h1><?php echo htmlspecialchars($rec['title']); ?></h1>
+                            <p>
+                                Auteur: <?php echo htmlspecialchars($rec['author']); ?> <br>
+                                Date de création: <?php echo htmlspecialchars($rec['creation_date']); ?> <br>
+                                Dernière date de modification: <?php echo htmlspecialchars($rec['modification_date']); ?> <br>
+                                <span class="content">
+                                    <?php echo nl2br(htmlspecialchars($rec['content'])); ?>
+                                </span>
+                                <br>
+                                <a href=<?php echo '"comments.php?id='.$rec['id'].'"'; ?> title="comments page"> <em>Zoom</em> </a>                        
+                            </p>
+                        </article>
+                <?php
+                    }
+                    $query->closeCursor();
+                ?>
             </section>
             
             <footer>
